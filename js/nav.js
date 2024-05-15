09fcfed5698d961e22f7de4380fa2929877c639c
Kazakova Anastasia <kutianina.n@yandex.ru>
2024-05-16 00:23:23 +0500

    Fix navigation

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,11 @@
+// Saved script just in case
 const currentUrl = window.location.href;
 const navPoints = document.querySelectorAll('.nav__point');
 
 navPoints.forEach(point => {
   if (currentUrl.includes(point.getAttribute('href'))) {
     point.classList.add('active');
+  } else {
+    point.classList.remove('active'); 
   }
 });
